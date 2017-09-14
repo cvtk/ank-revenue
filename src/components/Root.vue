e1797f9d929c8293625bde6a139789c0d67838e5
cvtk <olegcvetkov76@gmail.com>
2017-09-14 14:05:09 +0400

    noth(

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,9 +1,32 @@
 <template>
-  <p :class="$style.p">/</p>
+  <div :class="$style.root">
+    <div :class="$style._row">
+      <div :class="$style.root__portlet"></div>
+      <div :class="$style.root__portlet"></div>
+    </div>
+    <div :class="$style._row">
+      <div :class="$style.root__portlet"></div>
+      <div :class="$style.root__portlet"></div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" module>
-  .p { padding: 5px }
+  @import "../assets/styles/mixins.scss";
+
+  .root { /* */ }
+
+  ._row {
+    &:after { @include clearfix }
+    margin-left: -15px;
+    margin-right: -15px;
+  }
+
+  .root__portlet {
+    width: 50%;
+    float: left;
+  }
+  
 </style>
 
 <script>
